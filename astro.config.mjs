21037b3b543d27affac555f38a89a2ca73ae5269
JohnAntz <johnantzoulisvazquez@outlook.com>
2026-07-09 20:45:04 +0200

    Custom domain configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://JohnAntz.github.io",
-  base: "/johnantz-blog",
+  site: "blog.johnantz.dev",
+
   integrations: [mdx(), sitemap()],
 
   fonts: [
